4f2c76cd09814a2b3468af4b390041c6c432d1d8
Kumar-Aditya-Pratap <128625213+Kumar-Aditya-Pratap@users.noreply.github.com>
2025-12-17 11:29:16 +0530

    Task4: add base PaymentService

---
+++ Task4/Task4-app/Task4-app/PaymentServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_app
+{
+    class PaymentServices
+    {
+        public class PaymentService
+        {
+            public bool ProcessPayment(object payment)
+            {
+                // base logic
+                return true;
+            }
+        }
+
+    }
+}
